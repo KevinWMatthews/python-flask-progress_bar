c8ee5a895afdf8e93b43e1247366e43f45e73e66
Kevin Matthews <kmatthews@cyberdata.net>
2017-01-13 19:01:00 -0800

    Add Jijna2 templating stuff.

--- progress_bar.py
+++ progress_bar.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('progress_bar.html')
+    return render_template('progress_bar.html', progress=22)
 
 if __name__ == '__main__':
     app.run(debug = True)
